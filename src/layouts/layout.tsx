e844821618d160095b92a247fd7c995bee3b9e1b
devggui <devguiet@gmail.com>
2024-04-18 09:24:38 -0300

    refactored: remove unsed packages

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,13 +1,11 @@
 import { Header } from "@/components/header"
-import { Toaster } from "@/components/ui/sonner"
 import { Outlet } from "react-router-dom"
 
 export default function Layout() {
   return (
     <div className="flex flex-1 flex-col min-h-screen bg-secondary">
       <Header />
-      <Outlet />  
-      <Toaster />                      
+      <Outlet />        
     </div>
   )
 }
